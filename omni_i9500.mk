31a382a236e47c43737a5a3a8c659b6499e68e86
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-07-28 20:57:58 +0200

    Add initial omni support

--- omni_i9500.mk
+++ omni_i9500.mk
@@ -1,16 +1,15 @@
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := cm_i9500
+PRODUCT_NAME := omni_i9500
 PRODUCT_DEVICE := i9500
+PRODUCT_BRAND := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=GT-I9500 \
